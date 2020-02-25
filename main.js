79d378fe821e7bc17ef6ad5711a59e539cf2386c
Calo Corp <marc.calo@codeimmersives.com>
2020-02-25 11:59:41 -0500

    help solving

--- main.js
+++ main.js
@@ -13,9 +13,20 @@ function changeToInitials() {
 function doubleOdd() {
 }
 
-function upperCaseFirstLetters() {
+function upperCaseFirstLetters(words) {
+  const uppercased = [];
+  
+  for (let i=0; i < words.length; i++){
+    
+    const word = words[i];
+    
+    const upper = word[0].toUpperCase() + word.slice(1).toLowerCase();
+    
+    uppercased.push(upper);
+    
+  }
+  return uppercased
 }
-
 function add1ToLeft() {
 }
 
